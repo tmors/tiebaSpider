aa5d8b095ada1c934b14b75ff38f21f18fb5b218
Dust <398080994@163.com>
2017-02-25 13:20:58 +0800

    save

--- spider.py
+++ spider.py
@@ -10,34 +10,33 @@ class user:
     postNumb = 0
     def __init__(self):
         pass
-    def __init__(self,userAge,postNum):
-        self.userAge = userAge
-        self.postNumb = postNum
+
+class postContent:
+    #content user replied
+    content = ""
+    #user who post the content
+    author = user()
+    #content inner content
+    innerContent = []
+    def __init__(self):
         pass
 
 class post:
     title = ""
     href = ""
-    author = user(0,0)
+    author = user()
+    #list of postContent
+    posContentList = []
     def __init__(self):
         pass
-    def __init__(self,titleValue,hrefValue):
+    def __init__(self,hrefValue,titleValue):
         self.title = titleValue
         self.href = hrefValue
         pass
-    def setTitle(self,titleValue):
-        self.title = titleValue
-        pass
-    def setHref(self,hrefValue):
-        self.href = hrefValue
-        pass
-    def getTitle(self):
-        return self.title
-    def getHref(self):
-        return self.href
-
 def buildConnection(url):
-    httpPoll = urllib3.HTTPConnectionPool(url, maxsize=1)
+
+    # httpPoll = urllib3.HTTPConnectionPool(url, maxsize=1)
+    httpPoll = urllib3.PoolManager()
     return httpPoll
 
 def sendRequest(connection,url,fields):
@@ -45,9 +44,10 @@ def sendRequest(connection,url,fields):
     return page
 
 def main():
+    baseUrl = "http://tieba.baidu.com"
     httpPoll = buildConnection("tieba.baidu.com")
     fields={"kw":"花泽香菜"}
-    page = sendRequest(httpPoll,"/f",fields=fields)
+    page = sendRequest(httpPoll,baseUrl+"/f",fields=fields)
     content = page.data.decode("utf-8")
 
 
@@ -62,9 +62,27 @@ def main():
     for i in posts:
         href = hrefPattern.findall(i)
         title = titlePattern.findall(i)
-        curPost = post(href,title)
+        curPost = post(href[0],title[0])
         postList.append(curPost)
 
+    contentPostedList = []
+    contentPostedPattern = re.compile(r"(?<=class=\"d_post_content j_d_post_content \">).*?(?=</div>)")
+    authorPostedPattern = re.compile(r"(?<=class=\"d_name\" data-field='{&quot;user_id&quot;:)\d+?(?=\")")
+    for i in postList:
+        pageInnerPost = sendRequest(httpPoll,baseUrl + i.href,fields={})
+        content = pageInnerPost.data.decode("utf-8")
+        #extract post content
+        curPostsContentList = contentPostedPattern.findall(content)
+        curPostAuthorIdList = authorPostedPattern.findall(content)
+        postContentList = []
+        for i,j in curPostsContentList,curPostAuthorIdList:
+            curPostContent = postContent()
+            curPostContent.content = i
+            curPostContent.author = j
+            postContentList.append(curPostContent)
+
+        i.posContentList = postContentList
+        print()
 
     print(page)
 
